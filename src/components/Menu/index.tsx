b880b0541e1b85abaffd0127b378ddd43057c322
amandafralve <amanda.fralve@gmail.com>
2026-07-28 15:07:10 -0300

    feat: menu de ações e estilo

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,26 @@
+
+import { HistoryIcon, House, Settings, Sun } from 'lucide-react'
+import styles from './styles.module.css'
+
+export function Menu() {
+    return (
+        <nav className={styles.menu}>
+            <a className={styles.menuLink} href="#">
+                <House />
+            </a>
+
+            <a className={styles.menuLink} href="#">
+                <HistoryIcon />
+            </a>
+
+            <a className={styles.menuLink} href="#">
+                <Settings />
+            </a>
+
+            <a className={styles.menuLink} href="#">
+                <Sun />
+            </a>
+            
+        </nav>
+    )
+}
