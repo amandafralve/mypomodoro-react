897b7cb15af90a8fc218d8624f92502f9649e59c
amandafralve <amanda.fralve@gmail.com>
2026-07-29 14:48:18 -0300

    feat: adiciona estático de ciclos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from './components/Container';
 import { Countdown } from './components/Countdown';
+import { Cycles } from './components/Cycles';
 import { Input } from './components/Input';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -27,13 +28,15 @@ export function App(){
                     <div className="formRow">
                         <Input id='meuInput' labelText='Task' type='text' placeholder='Type your task'/>
                     </div>
+
                     <div className="formRow">
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
-                    <div className="formRow">
-                        <p>Ciclos</p>
-                        <p>00000</p>
+
+                    <div>
+                        <Cycles/>
                     </div>
+                    
                     <div className="formRow">
                         <button>Envia</button>
                     </div>
---
+++ src/components/Cycles/index.tsx
@@ -0,0 +1,23 @@
+import styles from "./styles.module.css"
+
+export function Cycles(){
+    return (
+        <>
+            <div className={styles.cycles}>
+                <span>Ciclos:</span>
+                <div className={styles.cyclesDot}>
+                    <div className={`${styles.cycleDot} ${styles.workTime} `}></div>
+                    <div className={`${styles.cycleDot} ${styles.shortBreakTime} `}></div>
+                    <div className={`${styles.cycleDot} ${styles.workTime} `}></div>
+                    <div className={`${styles.cycleDot} ${styles.shortBreakTime} `}></div>
+                    <div className={`${styles.cycleDot} ${styles.workTime} `}></div>
+                    <div className={`${styles.cycleDot} ${styles.shortBreakTime} `}></div>
+                    <div className={`${styles.cycleDot} ${styles.workTime} `}></div>
+                    <div className={`${styles.cycleDot} ${styles.longBreakTime} `}></div>
+                    
+                   
+                </div>
+            </div>
+        </>
+    )
+}
