36ddf0da9e85b16ae5178380627390a511a55dd8
amandafralve <amanda.fralve@gmail.com>
2026-07-28 22:00:42 -0300

    feat: component input default

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from './components/Container';
 import { Countdown } from './components/Countdown';
+import { Input } from './components/Input';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import './styles/global.css'
@@ -19,6 +20,24 @@ export function App(){
 
             <Container>
                 <Countdown />
+            </Container>
+
+            <Container>
+                <form className='form' action="">
+                    <div className="formRow">
+                        <Input id='meuInput' labelText='Task' type='text' placeholder='Type your task'/>
+                    </div>
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>00000</p>
+                    </div>
+                    <div className="formRow">
+                        <button>Envia</button>
+                    </div>
+                </form>
             </Container>
             
         </>
