3214b2446f8b5a31f3acd945d3a226fb70452f2f
amandafralve <amanda.fralve@gmail.com>
2026-07-31 15:47:07 -0300

    style: background texture e dot grid + favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,8 @@
-import { PlayCircleIcon } from 'lucide-react';
-import { Button } from './components/Button';
-import { Container } from './components/Container';
-import { Countdown } from './components/Countdown';
-import { Cycles } from './components/Cycles';
-import { Input } from './components/Input';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
+import { Home } from './Pages/Home';
+
 import './styles/global.css'
 import './styles/theme.css'
-import { Footer } from './components/Footer';
-
 
 export function App(){
-    return ( 
-        <>  
-            <Container>
-                <Logo />
-            </Container>
-
-            <Container>
-                <Menu />
-            </Container>
-
-            <Container>
-                <Countdown />
-            </Container>
-
-            <Container>
-                <form className='form' action="">
-                    <div className="formRow">
-                        <Input id='meuInput' labelText='Task' type='text' placeholder='Type your task'/>
-                    </div>
-
-                    <div className="formRow">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-
-                    <div>
-                        <Cycles/>
-                    </div>
-
-                    <div className="formRow">
-                        <Button icon={<PlayCircleIcon />} color='green'/>
-                    </div>
-                </form>
-            </Container>
-
-            <Container>
-                <Footer></Footer>            
-            </Container>
-            
-        </>
-
-    );
+    return <Home/>;
 }
